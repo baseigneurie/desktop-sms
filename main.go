d102ba0f8db4d00e82b72468110692af6c3c4f21
Broc Seigneurie <baseigneurie@gmail.com>
2016-11-15 13:34:09 -0600

    Added dialer during lunchbreak

--- main.go
+++ main.go
@@ -7,18 +7,16 @@ import (
 func main() {
     err := ui.Main(func() {
         name := ui.NewEntry()
-        button := ui.NewButton("Greet")
-        greeting := ui.NewLabel("")
         box := ui.NewVerticalBox()
-        box.Append(ui.NewLabel("Enter your name:"), false)
+        dialer := getDialer()
+
+        box.Append(ui.NewLabel("Enter contact name:"), false)
         box.Append(name, false)
-        box.Append(button, false)
-        box.Append(greeting, false)
-        window := ui.NewWindow("Hello", 200, 100, false)
+
+        box.Append(dialer, false)
+        window := ui.NewWindow("Hello", 300, 200, false)
         window.SetChild(box)
-        button.OnClicked(func(*ui.Button) {
-            greeting.SetText("Hello, " + name.Text() + "!")
-        })
+
         window.OnClosing(func(*ui.Window) bool {
             ui.Quit()
             return true
@@ -28,4 +26,49 @@ func main() {
     if err != nil {
         panic(err)
     }
+}
+
+func getDialer() ui.Control {
+  topRow := ui.NewHorizontalBox()
+  midRow := ui.NewHorizontalBox()
+  botRow := ui.NewHorizontalBox()
+  zeroRow := ui.NewHorizontalBox()
+  fullBox := ui.NewVerticalBox()
+
+  bt1 := ui.NewButton("1")
+  bt2 := ui.NewButton("2")
+  bt3 := ui.NewButton("3")
+  bt4 := ui.NewButton("4")
+  bt5 := ui.NewButton("5")
+  bt6 := ui.NewButton("6")
+  bt7 := ui.NewButton("7")
+  bt8 := ui.NewButton("8")
+  bt9 := ui.NewButton("9")
+  bt0 := ui.NewButton("0")
+
+  // Top Row
+  topRow.Append(bt1, true)
+  topRow.Append(bt2, true)
+  topRow.Append(bt3, true)
+
+  // Middle Row
+  midRow.Append(bt4, true)
+  midRow.Append(bt5, true)
+  midRow.Append(bt6, true)
+
+  // Bottom Row
+  botRow.Append(bt7, true)
+  botRow.Append(bt8, true)
+  botRow.Append(bt9, true)
+
+  //Zero Row
+  zeroRow.Append(bt0, true)
+
+  // Buttons container
+  fullBox.Append(topRow, true)
+  fullBox.Append(midRow, true)
+  fullBox.Append(botRow, true)
+  fullBox.Append(zeroRow, true)
+
+  return fullBox
 }
